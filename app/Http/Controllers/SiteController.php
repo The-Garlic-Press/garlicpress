0b8d34203e58a1055ecf4100cf4876905f1c2185
Daniel Supernault <danielsupernault@gmail.com>
2018-02-17 15:34:43 -0700

    Update SiteController

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class SiteController extends Controller
 {
     public function home()
     {
-      return view('site.home');
+      $posts = Post::published()->newestFirst()->paginate(4);
+      return view('site.home', compact('posts'));
     }
 }
