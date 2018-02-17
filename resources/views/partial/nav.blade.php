386b83892c098fa1ee2ffb2733942efa6de6f1e7
Daniel Supernault <danielsupernault@gmail.com>
2018-02-16 19:55:13 -0700

    Add basic design

---
+++ resources/views/partial/nav.blade.php
@@ -0,0 +1,23 @@
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a href="https://grlc.press"><img src="/images/biglogo.png" width="268px" height="50px" style="padding-rght: 20px;"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="https://grlc.press">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://grlc.press/Announcing-The-Garlic-Press">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/contact.php">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'SiteController@home');
 
 
 Route::group(['prefix' => 'admin'], function () {
