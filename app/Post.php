5ef6e3a0efef1d9eca0c08b274c37cfd31ab2ee6
Daniel Supernault <danielsupernault@gmail.com>
2018-02-17 15:34:27 -0700

    Update Post model

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends \TCG\Voyager\Models\Post
 {
-    //
+  public function scopeNewestFirst($query)
+  {
+    return $query->orderBy('created_at', 'desc');
+  }
+
+  public function url()
+  {
+    $year = $this->created_at->year;
+    $month = $this->created_at->format('m');
+    return url("/{$year}/{$month}/{$this->slug}");
+  }
+
+  public function author()
+  {
+    return $this->belongsTo(User::class, 'author_id', 'id');
+  }
 }
