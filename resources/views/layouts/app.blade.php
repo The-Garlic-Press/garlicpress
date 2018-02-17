8b03ed6d9a8b7e731e30c0886204267ac514c6ef
Daniel Supernault <danielsupernault@gmail.com>
2018-02-17 15:35:11 -0700

    Add footer blade template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,8 @@
         @include('partial.nav')
 
         @yield('content')
+
+        @include('partial.footer')
     </div>
 
     <!-- Scripts -->
---
+++ resources/views/partial/footer.blade.php
@@ -0,0 +1,7 @@
+   <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; grlc.press 2018</p>
+        <a href="/privacy.txt" target="_blank"><p class="m-0 text-center text-white">Privacy Policy</p></a>
+      </div>
+      <!-- /.container -->
+    </footer>
