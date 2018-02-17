796506f571251e35122fc33aea75ec335cdc3563
Daniel Supernault <danielsupernault@gmail.com>
2018-02-17 15:34:35 -0700

    Update PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
-    //
+    public function show(Request $request, $year, $month, $slug)
+    {
+      $post = Post::whereSlug($slug)->whereYear('created_at', $year)->whereMonth('created_at', $month)->published()->firstOrFail();
+      return view('post.show', compact('post'));
+    }
 }
